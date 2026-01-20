c5e346de6fc4bc40979d5aee1ebd8bd50cb399d7
jonabreslow <jonahbreslow@gmail.com>
2026-01-20 23:15:57 +0100

    fixing Power Level 10k typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,6 +30,6 @@ unset doIt;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
 
 # PL10K
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/pLLLLowerlevel10k > /dev/null 2>&1
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k > /dev/null 2>&1
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null 2>&1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null 2>&1
